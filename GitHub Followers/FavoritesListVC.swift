91a2dc01812b96e94a26dd2a532f01b9c2842c2f
Alex Cruz <alexcruz970@yahoo.ca>
2023-04-03 01:30:19 -0400

    Added Search and Favorites List  View Controllers

---
+++ GitHub Followers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GitHub Followers
+//
+//  Created by Alex Cruz on 2023-04-03.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+    }
+}
--- GitHub Followers/SearchVC.swift
+++ GitHub Followers/SearchVC.swift
@@ -1,20 +1,18 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GitHub Followers
 //
-//  Created by Alex Cruz on 2023-03-26.
+//  Created by Alex Cruz on 2023-04-03.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
-        print("hello world")
+        view.backgroundColor = .systemPink
     }
-
-
 }
-
