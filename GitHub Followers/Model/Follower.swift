cbb65edaff003e4a506610c7dc0dafb178ca80ac
Alex Cruz <alexcruz970@yahoo.ca>
2023-04-05 00:55:20 -0400

    Created Network Call to get followers

---
+++ GitHub Followers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHub Followers
+//
+//  Created by Alex Cruz on 2023-04-04.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
